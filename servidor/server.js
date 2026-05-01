fd70013d66a5cfa1c3bcdcfb417847c8ef4bc232
Mrc0345 <marcusviniciusor09@gmail.com>
2026-05-01 16:12:14 -0300

    adicionando back-end na pasta services com o arquivo api.js também realizando "npm install express cors" na pasta servidor para instalar as dependencias do express

---
+++ servidor/server.js
@@ -0,0 +1,20 @@
+// servidor/server.js
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+// Middlewares básicos
+app.use(cors()); // Permite que o Front-end comunique com o Back-end
+app.use(express.json()); // Permite que o servidor entenda requisições com corpo em JSON
+
+// Rota de teste
+app.get('/api/status', (req, res) => {
+    res.json({ mensagem: 'Servidor de Controle Financeiro rodando perfeitamente!' });
+});
+
+// Inicialização do servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
