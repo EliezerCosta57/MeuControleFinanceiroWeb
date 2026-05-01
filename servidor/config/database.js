4567095f295abccd3654af9329c2c31412d511b6
Mrc0345 <marcusviniciusor09@gmail.com>
2026-05-01 16:28:38 -0300

    adicionando arquivos db e database para banco de dados na pasta config arquivo transacaoController na pasta controllers transacao.js e usuario.js na pasta models pasta routes com transacoesRoutes

---
+++ servidor/config/database.js
@@ -0,0 +1,5 @@
+// logica do banco de dados - Wesley
+
+//O que vai ter: Um arquivo db.js ou database.js.
+
+// Para que serve: Fazer a conexão com o banco de dados (por exemplo, conectar ao MySQL ou PostgreSQL) para que o sistema consiga ler e gravar os dados financeiros.
