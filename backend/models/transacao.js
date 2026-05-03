429dca989ff5e9483ccc5ece26d2ce98fcfa04a2
Mrc0345 <marcusviniciusor09@gmail.com>
2026-05-02 22:32:51 -0300

    comentando apos confirmar ligacao com o back end em mensagem inseridas no codigo

--- backend/models/transacao.js
+++ backend/models/transacao.js
@@ -1,7 +1,7 @@
 // Simulando um banco de dados com uma lista (Array)
 let transacoes = [
-    { id: 1, descricao: 'Salário', valor: 5000, tipo: 'receita', data: '2026-05-01' },
-    { id: 2, descricao: 'Conta de Luz', valor: 150, tipo: 'despesa', data: '2026-05-05' }
+  //  { id: 1, descricao: 'Salário', valor: 5000, tipo: 'receita', data: '2026-05-01' },
+   // { id: 2, descricao: 'Conta de Luz', valor: 150, tipo: 'despesa', data: '2026-05-05' } - dados aleatorios para teste
 ];
 
 class Transacao {
