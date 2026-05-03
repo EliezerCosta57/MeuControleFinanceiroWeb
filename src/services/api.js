ea20fcf201e2a4370a6962a98b15457d3043bad4
Mrc0345 <marcusviniciusor09@gmail.com>
2026-05-02 22:14:02 -0300

    nova alteração no api.js na pasta services. correção para testar a conexão do back-end com o front-end

--- src/services/api.js
+++ src/services/api.js
@@ -4,19 +4,16 @@ const api = axios.create({
   baseURL: "http://localhost:3000/api"
 });
 
-// LISTAR
 export async function listarTransacoes() {
   const resposta = await api.get("/transacoes");
   return resposta.data;
 }
 
-// CRIAR
 export async function criarTransacao(transacao) {
   const resposta = await api.post("/transacoes", transacao);
   return resposta.data;
 }
 
-// DELETAR
 export async function deletarTransacao(id) {
   const resposta = await api.delete(`/transacoes/${id}`);
   return resposta.data;
